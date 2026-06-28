b3da9015eee266cb76bc75e46e67befdcd295c6a
Tanisha Trivedi <tanisha6704@gmail.com>
2026-06-28 18:46:39 +0530

    WIP: frontend landing page navbar is not completed

---
+++ frontend/src/components/LandingPage/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <h1>Sortify</h1>
+
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
