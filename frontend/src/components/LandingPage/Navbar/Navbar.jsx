2bfd1a260768d81a4dec8ed0e41e316472803682
Tanisha Trivedi <tanisha6704@gmail.com>
2026-06-29 10:59:14 +0530

    Landing page Navbar styling is completed

--- frontend/src/components/LandingPage/Navbar/Navbar.jsx
+++ frontend/src/components/LandingPage/Navbar/Navbar.jsx
@@ -5,11 +5,17 @@ const Navbar = () => {
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        <h1>Sortify</h1>
+        {/* website logo */}
+        <div className={styles.logo}>
+          <img className={styles.logoimg} src="/logo.png" alt="website logo" />
+          <h1>Sortify</h1>
+        </div>
 
-        <ul>
-          <li></li>
-        </ul>
+        {/* signup or login button */}
+        <div className={styles.buttons}>
+          <button className={styles.login}>Login</button>
+          <button className={styles.signup}>Sign Up</button>
+        </div>
       </nav>
     </header>
   );
