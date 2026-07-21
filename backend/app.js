f12a7032212dc804bdae69d9c3cda3d1339e6f77
Tanisha Trivedi <tanisha6704@gmail.com>
2026-07-21 14:02:51 +0530

    Now i have completed my backend part and made all the api now after this i will connect it to frontend

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const db = require("./db")
+const cors = require("cors")
 
 const app = express();
 db()
 
 // middleware
 app.use(express.json())
+app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 
